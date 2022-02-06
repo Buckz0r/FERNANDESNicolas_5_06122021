3655c6cbf92f403f575bd763d61cd686bd14e616
FERNANDES Nicolas <fernandes.nicolas@hotmail.com>
2022-02-07 00:44:08 +0100

    Ajout de commentaire du fichier script.js

--- front/js/script.js
+++ front/js/script.js
@@ -1,9 +1,13 @@
+// On envoie une requête GET pour afficher tous les produits
+
 fetch("http://localhost:3000/api/products")
     .then(function(res) {
         if(res.ok) {
             return res.json();
         }
     })
+    // Fonction pour afficher les détails des produits
+
     .then(function(value) {
         console.log(value)
         var section = document.getElementById("items");
